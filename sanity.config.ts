82d99106cbea49445f174ce6a69acf8fbdc471fb
poncho678 <vincent.tollens@googlemail.com>
2023-06-08 09:17:36 +0200

    create singletons dynamically

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,31 +4,30 @@ import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 import singletons from './utils/singletons.helper'
 
-const singletonActions = new Set(["publish", "discardChanges", "restore"])  
+const singletonActions = new Set(['publish', 'discardChanges', 'restore'])
 const singletonTypes = new Set(['about', 'contact', 'imprint', 'privacy'])
+const singletonArray = Object.values(singletons)
 
-export default defineConfig ({
+export default defineConfig({
   name: 'default',
   title: 'pauldrey',
   projectId: process.env.SANITY_STUDIO_PROJECT_ID,
   dataset: 'production',
   plugins: [
     deskTool({
-      structure: (S) => 
+      structure: (S) =>
         S.list()
-          .title("Pages")
+          .title('Pages')
           .items([
-            S.listItem()
-              .title("About")
-              .id("about")
-              .child(
-                S.document()
-                  .schemaType("about")
-                  .documentId("about")
-              ),
+            ...singletonArray.map((x) =>
+              S.listItem()
+                .title(x.title)
+                .id(x.name)
+                .child(S.document().schemaType(x.name).documentId(x.title))
+            ),
 
             // Regular document types
-            S.documentTypeListItem("projects").title("Projects"),
+            S.documentTypeListItem('projects').title('Projects'),
           ]),
     }),
     visionTool(),
@@ -36,14 +35,12 @@ export default defineConfig ({
 
   schema: {
     types: schemaTypes,
-    templates: (templates) =>
-      templates.filter(({ schemaType }) => !singletonTypes.has(schemaType)),
+    templates: (templates) => templates.filter(({schemaType}) => !singletonTypes.has(schemaType)),
   },
   document: {
     actions: (input, context) =>
-    singletonTypes.has(context.schemaType)
-      ? input.filter(({ action }) => action && singletonActions.has(action))
-      : input,
-  }
+      singletonTypes.has(context.schemaType)
+        ? input.filter(({action}) => action && singletonActions.has(action))
+        : input,
+  },
 })
-
