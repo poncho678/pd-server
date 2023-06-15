43e58b785577558c31f864ceaece611aaebab8e9
poncho678 <vincent.tollens@googlemail.com>
2023-06-15 23:42:18 +0200

    updated schema

--- schemas/about.schema.ts
+++ schemas/about.schema.ts
@@ -6,6 +6,18 @@ export default {
   title: singletons['about'].title,
   fields: [
     {
+      name: 'siteTitle',
+      title: 'Site Title',
+      type: 'string',
+    },
+    {
+      name: 'siteDescription',
+      title: 'Site Description',
+      type: 'text',
+      validation: (Rule: any) =>
+        Rule.max(155).warning('Description should not be longer than 155 characters.'),
+    },
+    {
       name: 'aboutText',
       title: 'About Text',
       type: 'array',
--- schemas/projects.schema.ts
+++ schemas/projects.schema.ts
@@ -1,12 +1,101 @@
 export default {
-    name: 'projects',
-    type: 'document',
-    title: 'Projects',
-    fields: [
+  name: 'projects',
+  type: 'document',
+  title: 'Projects',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+    },
+    {
+      title: 'Slug',
+      name: 'slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        slugify: (input: string) => input.toLowerCase().replace(/\s+/g, '-').slice(0, 200),
+      },
+      validation: (Rule: any) =>
+        Rule.required().warning(
+          'A slug is required. Please generate a slug before you publish a project.'
+        ),
+    },
+    {
+      title: 'Featured Image',
+      name: 'featuredImage',
+      type: 'image',
+      fields: [
         {
-            name: 'title',
-            type: 'string',
-            title: 'Title'
-        }
-    ]
+          title: 'Description',
+          name: 'description',
+          type: 'string',
+        },
+      ],
+      options: {
+        metadata: [
+          'blurhash', // Default: included
+          'lqip', // Default: included
+          'palette', // Default: included
+        ],
+      },
+    },
+    {
+      title: 'Poster',
+      name: 'poster',
+      type: 'image',
+      options: {
+        metadata: [
+          'blurhash', // Default: included
+          'lqip', // Default: included
+          'palette', // Default: included
+        ],
+      },
+    },
+    {
+      title: 'Trailer',
+      name: 'trailer',
+      description: '',
+      type: 'url',
+      validation: (Rule: any) =>
+        Rule.uri({
+          scheme: ['http', 'https'],
+        }).warning('Invalid. A valid URL starts with “http://” or “https://”, please try again.'),
+    },
+    {
+      title: 'Stills',
+      name: 'stills',
+      type: 'array',
+      of: [
+        {
+          type: 'image',
+          options: {
+            metadata: [
+              'blurhash', // Default: included
+              'lqip', // Default: included
+              'palette', // Default: included
+            ],
+          },
+          fields: [
+            {
+              title: 'Description',
+              name: 'description',
+              type: 'string',
+            },
+          ],
+        },
+      ],
+
+      options: {
+        layout: 'grid',
+      },
+    },
+    {
+      title: 'Published',
+      name: 'published',
+      description: 'Set to published when this project should be visible on the frontend',
+      type: 'boolean',
+      initialValue: true,
+    },
+  ],
 }
