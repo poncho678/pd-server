a268547c6fdd63868f9290916d322c82d2af7445
poncho678 <vincent.tollens@googlemail.com>
2023-07-06 14:07:44 +0200

    moved types and schemas into separate arrays

--- schemas/index.ts
+++ schemas/index.ts
@@ -7,14 +7,9 @@ import projects from './projects.schema'
 import {stillsType} from './customTypes/stillsType'
 import {videoType} from './customTypes/videoType'
 import {synopsisType} from './customTypes/synopsisType'
+import {loglineType} from './customTypes/loglineType'
 
-export const schemaTypes = [
-  about,
-  contact,
-  imprint,
-  privacy,
-  projects,
-  stillsType,
-  videoType,
-  synopsisType,
-]
+const schemas = [about, contact, imprint, privacy, projects]
+const types = [loglineType, stillsType, synopsisType, videoType]
+
+export const schemaTypes = [...schemas, ...types]
