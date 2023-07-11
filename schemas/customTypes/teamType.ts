c49eb9c9268a42c41dbd858ddaa4de329d62e971
poncho678 <vincent.tollens@googlemail.com>
2023-07-11 13:33:49 +0200

    added team schemaType

--- schemas/customTypes/teamType.ts
+++ schemas/customTypes/teamType.ts
@@ -1,8 +1,36 @@
+import {SparklesIcon} from '@sanity/icons'
 import {defineField, defineType} from 'sanity'
 
 export const teamType = defineType({
   name: 'team',
   title: 'Team',
   type: 'object',
-  fields: [],
+  fields: [
+    defineField({
+      name: 'poster',
+      title: 'Poster',
+      type: 'image',
+    }),
+    defineField({
+      name: 'main',
+      title: 'Main',
+      type: 'array',
+      of: [
+        defineField({
+          name: 'position',
+          title: 'Position',
+          type: 'string',
+        }),
+      ],
+    }),
+  ],
+  icon: SparklesIcon,
+  preview: {
+    prepare() {
+      return {
+        title: 'Team',
+        subtitle: 'List',
+      }
+    },
+  },
 })
